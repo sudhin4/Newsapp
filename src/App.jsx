386d188d5e8777a1c1aedc337df05536cbc1299c
sudhin4 <sudhinnaveen38@gmail.com>
2025-04-29 00:40:45 +0530

    small changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,41 @@
 import Mappingnews from "./Component/Mappingnews";
 import Newspage from "./Component/Newspage";
 import Header from "./Component/Header/Header";
-function App(){
+function App() {
+  const [deferredPrompt, setDeferredPrompt] = useState(null);
+  const [showInstallBtn, setShowInstallBtn] = useState(false);
+  useEffect(() => {
+    window.addEventListener("beforeinstallprompt", (e) => {
+      e.preventDefault();
+      setDeferredPrompt(e);
+      setShowInstallBtn(true);
+    });
+  }, []);
+  const handleInstallClick = () => {
+    if (deferredPrompt) {
+      deferredPrompt.prompt();
+      deferredPrompt.userChoice.then((choiceResult) => {
+        if (choiceResult.outcome === "accepted") {
+          console.log("App installed");
+        } else {
+          console.log("App installation rejected");
+        }
+        setDeferredPrompt(null);
+        setShowInstallBtn(false);
+      });
+    }
+  };
   return (
     <>
-    <Header/>
-  <Mappingnews/>
+      <Header installfun={handleInstallClick} />
+      <Mappingnews />
+      {/* <div>
+        <h1>App</h1>
+        {showInstallBtn && (
+          <button onClick={handleInstallClick}>Install App</button>
+        )}
+      </div> */}
     </>
-  )
+  );
 }
 export default App;
--- src/Component/Header/Header.jsx
+++ src/Component/Header/Header.jsx
@@ -2,13 +2,13 @@ import '../Header/Header.css'
 import { MdDownload } from "react-icons/md";
 
 
-function Header(){
+function Header({installfun}){
     return(
         <>
         <div className="whole_header_component">
             <div className="content">
                 <h2 className="Logo_name">News App</h2>
-                <button className='download_btn'><MdDownload className='download_icon'/>Download App</button>
+                <button className='download_btn' onClick={installfun}><MdDownload className='download_icon'/>Download App</button>
             </div>
         </div>
         </>
