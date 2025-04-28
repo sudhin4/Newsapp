32e642ed157976d47af23c23ecb8d8d77f4783e7
sudhin4 <sudhinnaveen38@gmail.com>
2025-04-29 00:27:01 +0530

    push

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,33 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vite.dev/config/
+import { VitePWA } from 'vite-plugin-pwa';
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      includeAssets: ['favicon.svg', 'News Logo.png'],
+      manifest: {
+        name: 'Newsapp',
+        short_name: 'Newsapp',
+        start_url: '/',
+        display: 'standalone',
+        background_color: '#000000',
+        theme_color: '#1d4ed8',
+        icons: [
+          {
+            src: 'News Logo.png',
+            sizes: '192x192',
+            type: 'image/png',
+          },
+          {
+            src: 'News Logo.png',
+            sizes: '512x512',
+            type: 'image/png',
+          },
+        ],
+      },
+    }),
+  ],
+});
